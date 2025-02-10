92a6b197fcf67abf161db7766d9b6dc9afc2d614
driad11 <tegt.mika@gmail.com>
2025-02-10 17:04:02 +0100

    Add files via upload
    added multi page support... hopefully

---
+++ pages/deutsch.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Willkommen!")
+st.write("Dies ist die deutsche Version der Seite.")
---
+++ pages/english.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Welcome!")
+st.write("This is the English version of the page.")
