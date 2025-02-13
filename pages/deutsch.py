db3f8d70f977ec9a0f3b07718fbc34ebf48825b0
driad11 <tegt.mika@gmail.com>
2025-02-13 15:10:58 +0100

    Update deutsch.py

--- pages/deutsch.py
+++ pages/deutsch.py
@@ -1,26 +1,33 @@
 import streamlit as st
+import requests
+import PIL import Image
+
+img_contact_form = Image.open("images/logo")
+
 
 st.title("Willkommen!")
-st.image("D:\Downloads\logo.jpg")
-st.write("""Angebot
-Egal, ob eine einfache Renovierung, ein Umbau, ein Anbau oder eine Sanierung: 
-Wir begleiten den Prozess vom Anfang der Planung bis zum Abschluss der Maßnahmen 
-Wir unterstützen hinsichtlich der Entscheidung, welche Maßnahmen baulich sinnvoll, geboten und im Rahmen eines Budgets wirtschaftlich machbar sind. 
-Wir helfen auch bei der Bestimmung des notwendigen Budgets durch eine detaillierte Kostenschätzung
-Wir erstellen oder unterstützen bei der Erstellung eines Bauablaufplans und sorgen für die Überwachung und Erreichung der gesetzten Ziele.
-Wir unterstützen, soweit durch Baumaßnahmen rechtliche Belange betroffen sind (z. B. notwendige Baugenehmigung) und können bei Bedarf auch Fördermöglichkeiten prüfen
-  Soweit dritte Unternehmen für Bauleistungen beauftragt werden sollen/müssen, übernehmen wir folgende Leistungen:  
-  die Einholung von Angeboten und die Beauftragung in Ihrem Namen, 
-  die Abrechnung und Kostenkontrolle,
-  die Bauüberwachung der Leistungen nach vorheriger Zieldefinition
- wir erstellen ein Bautagebuch als Dokumentation des Baufortschritts und zur Überwachung der Kosten
- bei Bedarf können wir einzelne Baumaßnahmen persönlich übernehmen
 
-Was ist 2πr?
-2πr hat seine Ursprünge in der Bauplanung und Bauumsetzung. Es geht um den umfassenden Lebenszyklus eines Gebäudes, beginnend bei der Idee, über die Planung und die Umsetzung, bis hin zur erneuten Wiederherstellung, einer Sanierung oder Erweiterung, um sich ändernden Wohnbedürfnissen anzupassen. Wir durften feststellen, dass viele der Immobilien in Ungarn den modernen und westeuropäischen Wohnbedürfnissen noch nicht vollständig entsprechen. Hier setzen wir an und begleiten Bauherren, die nach Ungarn ausgewandert sind oder auswandern wollen, auf einem Stück dieses Lebenszyklusses hinein in ein neues Leben.
-Für uns ist es wichtig „harte“ Entscheidungsfaktoren wie Kosten, Bautechnik und Ausführungsqualität, aber auch Gestaltung, Schönheit und Zuverlässigkeit in den Bauprozess zu integrieren.
+with st.container():
+    st.write("---")
+    st.header("Angebot")
+    st.write("##")
+    image_column, text_column = ((1,2))
+    with image_column:
+        st.image(img_contact_form)
 
-Wir greifen auf ein Netzwerk an zuverlässigen und günstigen Zulieferern und Handwerkerfirmen zu. Viele von ihnen kennen wir aus eigener Erfahrung. In verschiedenen Bauprojekten, Sanierungen und Modernisierungen innerhalb der Baranya haben wir bereits beratend, aber auch in der Umsetzung dieses Wissen und Können an unsere Kunden weitergeben dürfen.
+    with text_column:
+        st.write("""
+    Egal, ob eine einfache Renovierung, ein Umbau, ein Anbau oder eine Sanierung: 
+    Wir begleiten den Prozess vom Anfang der Planung bis zum Abschluss der Maßnahmen 
+    Wir unterstützen hinsichtlich der Entscheidung, welche Maßnahmen baulich sinnvoll, geboten und im Rahmen eines Budgets wirtschaftlich machbar sind. 
+    Wir helfen auch bei der Bestimmung des notwendigen Budgets durch eine detaillierte Kostenschätzung
+    Wir erstellen oder unterstützen bei der Erstellung eines Bauablaufplans und sorgen für die Überwachung und Erreichung der gesetzten Ziele.
+    Wir unterstützen, soweit durch Baumaßnahmen rechtliche Belange betroffen sind (z. B. notwendige Baugenehmigung) und können bei Bedarf auch Fördermöglichkeiten prüfen
+      Soweit dritte Unternehmen für Bauleistungen beauftragt werden sollen/müssen, übernehmen wir folgende Leistungen:  
+      die Einholung von Angeboten und die Beauftragung in Ihrem Namen, 
+      die Abrechnung und Kostenkontrolle,
+      die Bauüberwachung der Leistungen nach vorheriger Zieldefinition
+     wir erstellen ein Bautagebuch als Dokumentation des Baufortschritts und zur Überwachung der Kosten
+     bei Bedarf können wir einzelne Baumaßnahmen persönlich übernehmen """)
 
-         
-         """)
+
