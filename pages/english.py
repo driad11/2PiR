5e59bdce9888c023eadb24d11bad5d669dee0e76
driad11 <tegt.mika@gmail.com>
2025-02-14 11:25:48 +0100

    Delete pages directory

--- pages/english.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Welcome!")
-st.write("This is the English version of the page.")
--- pages/magyar.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Üdvözöljük!")
-st.write("Ez az oldal magyar verziója.")
