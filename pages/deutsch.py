e582b9f00f5d7534edbe4f308f9938c49bb4a4df
driad11 <tegt.mika@gmail.com>
2025-02-18 14:02:19 +0100

    Update deutsch.py

--- pages/deutsch.py
+++ pages/deutsch.py
@@ -13,8 +13,8 @@ st.markdown(
         /* Target the logo and force position updates */
         .logo-container {{
             position: absolute !important;
-            top: 10px !important;  /* Adjust as needed */
-            right: -20px !important; /* Adjust as needed */
+            top: 0px !important;  /* Adjust as needed */
+            right: 0px !important; /* Adjust as needed */
             transform: translate(-40, 0)
             width: 120px !important; /* Resize the logo */
             height: auto !important;
